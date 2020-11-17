ecf0f97c639594dceba0f313888f2717795729e4
yakshithahariram <haritharamreddy111@gmail.com>
2020-11-18 10:22:51 +1100

    Create IOS_job.py

---
+++ IOS_job.py
@@ -0,0 +1,11 @@
+# Example: ios_job.py
+# -------------------
+#
+#   a simple job file for the script above
+
+from pyats.easypy import run
+
+def main():
+
+    # run api launches a testscript as an individual task.
+    run('connectivity_check.py')
