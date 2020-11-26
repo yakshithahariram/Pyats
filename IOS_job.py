0764d398014486a80fe18e59fe4b7a089c650bf1
yakshithahariram <haritharamreddy111@gmail.com>
2020-11-26 12:28:25 +1100

    Update IOS_job.py

--- IOS_job.py
+++ IOS_job.py
@@ -1,11 +1,19 @@
-# Example: ios_job.py
-# -------------------
-#
-#   a simple job file for the script above
+"""
+<PYATS_JOBFILE>
+"""
+# To run the job:
+# pyats run job basic_example_job.py
+# Description: This example shows the basic functionality of pyats
+#              with few passing tests
 
+import os
 from pyats.easypy import run
 
+# All run() must be inside a main function
 def main():
+    # Find the location of the script in relation to the job file
+    test_path = os.path.dirname(os.path.abspath(__file__))
+    testscript = os.path.join(test_path, 'Connectivity_check_script.py')
 
-    # run api launches a testscript as an individual task.
-    run('Connectivity_check.py')
+    # Execute the testscript
+    run(testscript=testscript)
