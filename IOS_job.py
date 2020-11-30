a906a8ef9538f569d5dc031b2c9414070cece504
yakshithahariram <haritharamreddy111@gmail.com>
2020-11-30 17:47:00 +1100

    Update IOS_job.py

--- IOS_job.py
+++ IOS_job.py
@@ -1,17 +1,7 @@
-"""
-<PYATS_JOBFILE>
-"""
-# To run the job:
-# pyats run job basic_example_job.py
-# Description: This example shows the basic functionality of pyats
-#              with few passing tests
-
 import os
 from pyats.easypy import run
 
-# All run() must be inside a main function
 def main():
-    # Find the location of the script in relation to the job file
     test_path = os.path.dirname(os.path.abspath(__file__))
     testscript = os.path.join(test_path, 'telnet_script.py')
 
